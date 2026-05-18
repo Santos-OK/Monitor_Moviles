5ed1dd898e69d302db72d5681e9b53005ed9c6c6
Santos-OK <0262142@up.edu.mx>
2026-05-18 16:59:37 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,31 @@
   </ion-app>
 </template>
 
-<script setup lang="ts">
+<script>
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+export default {
+  name: "App",
+  components: { IonApp, IonRouterOutlet },
+
+  created() {
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, "omar@santos.com", "omarsantos333")
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        if (user == null) {
+          console.log("no autenticado")
+        }
+        else {
+          console.log("usuario autenticado", user);
+        }
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  },
+}
 </script>
--- src/main.js
+++ src/main.js
@@ -29,13 +29,32 @@ import '@ionic/vue/css/display.css';
 
 /* @import '@ionic/vue/css/palettes/dark.always.css'; */
 /* @import '@ionic/vue/css/palettes/dark.class.css'; */
-import '@ionic/vue/css/palettes/dark.system.css';
+/*import '@ionic/vue/css/palettes/dark.system.css';*/
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDGOzv6-h6ovNyQtSC8bXlCM6DgyvN2PGo",
+  authDomain: "moviles-final-2f6f0.firebaseapp.com",
+  projectId: "moviles-final-2f6f0",
+  storageBucket: "moviles-final-2f6f0.firebasestorage.app",
+  messagingSenderId: "162994451595",
+  appId: "1:162994451595:web:5e740dec824f4f713ad08e"
+};
+
+// Initialize Firebase
+const appFirebase = initializeApp(firebaseConfig);
 
 /* Theme variables */
 import './theme/variables.css';
 
 const app = createApp(App)
   .use(IonicVue)
+  .provide(appFirebase)
   .use(router);
 
 router.isReady().then(() => {
